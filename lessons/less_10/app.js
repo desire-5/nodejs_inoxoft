2f5a38573a7059e863d5a6d0ebb35537f1534e24
Vasyl Vasylkiv <desire@MacBook-Pro.local>
2021-09-11 10:15:23 +0300

    task 10

--- lessons/less_10/app.js
+++ lessons/less_10/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const expressFileUpload = require('express-fileupload');
+
 require('dotenv').config();
 
 const app = express();
@@ -11,6 +13,7 @@ global.TextEncoder = require('util').TextEncoder;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(expressFileUpload());
 
 const {
     authRouter, userRouter, orderRouter,
